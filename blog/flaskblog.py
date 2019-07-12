6c81ab1eee6b2fec08cb89222467e95c874dcf9f
terriasxu <xuzewei@gmail.com>
2019-07-13 07:25:51 +0800

    add blog with flask

---
+++ blog/flaskblog.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts) 
+#enddef
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+#end def about()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+#end if
+
